9c3d5d990e9647638875b91d0160cfe5257a92d6
Philip Kjærsgaard - Lobe ApS <pk@lobe.dk>
2020-12-04 11:07:29 +0100

    get-url for chrome silent install

--- bootstrap-laptop.ps1
+++ bootstrap-laptop.ps1
@@ -50,9 +50,50 @@ try {
     Write-Host "[ERROR]" $PSItem.Exception.Message
 } 
 
+# Set RealTime to UTC
+$RegKey = "HKLM:\System\CurrentControlSet\Control\TimeZoneInformation"
+if (-Not(Test-Path "$RegKey")) {
+    New-Item -Path $RegKey -Force
+}
+Set-ItemProperty -Path $RegKey -Name "RealTimeIsUniversal" -Value 1
+
 # Disable Hyperlink warning in Office 16.0
 $RegKey = "HKCU:\Software\Policies\Microsoft\Office\16.0\Common\Security"
 if (-Not(Test-Path "$RegKey")) {
     New-Item -Path $RegKey -Force
 }
 Set-ItemProperty -Path $RegKey -Name "DisableHyperlinkWarning" -Value 1
+
+
+# curl and run silent install
+# our curl command, with basic authentication if $credentials provided
+function Get-Url {
+    param(
+        [string]$url, # e.g. "http://dl.google.com/chrome/install/375.126/chrome_installer.exe"
+        [string]$filepath, # e.g. "c:\temp\chrome_installer.exe"
+        [string]$credentials # e.g. "username:pass"
+    )
+
+    $client = New-Object System.Net.WebClient;
+
+    if ($credentials) {
+        $credentialsB64 = [System.Text.Encoding]::UTF8.GetBytes($credentials) ;
+        $credentialsB64 = [System.Convert]::ToBase64String($credentialsB64) ;    
+        $client.Headers.Add("Authorization", "Basic " + $credentialsB64) ;
+    }    
+
+    $client.DownloadFile($url, $filepath);
+}
+
+Get-Url http://dl.google.com/chrome/install/375.126/chrome_installer.exe c:\temp\chrome_installer.exe
+c:\temp\chrome_installer.exe /silent /install
+
+
+try {
+    # Remove local Office 365 user from Administrators
+    Remove-LocalGroupMember -SID $AdminGroupSID -Member AzureAD\$_mail -ErrorAction Stop
+} catch [Microsoft.PowerShell.Commands.RemoveLocalGroupMemberCommand] {
+    # Already in group
+    Write-Host "[ERROR] Could not remove user from admin group"
+} 
+
